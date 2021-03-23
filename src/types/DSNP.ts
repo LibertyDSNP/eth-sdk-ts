6dcb95024c9fca291405ebbcd75aac9bdc75dd7d
shannonwells <shannonwells@users.noreply.github.com>
2021-03-23 15:54:05 -0700

    add tests, use DSNP namespace, remove lib-privacy-js from package.lock

--- src/types/DSNP.ts
+++ src/types/DSNP.ts
@@ -1,119 +1,132 @@
 import { EthereumAddress } from "./Strings";
 
-/**
- * Broadcast: a public post
- */
-export interface DSNPBroadcast {
-  /** fromAddress	ID of the sender	bytes20 */
-  fromAddress: EthereumAddress;
-  /** messageID	keccak-256 hash of content stored at URI	bytes32 */
-  messageID: Uint8Array;
-  /** uri	content URI	string */
-  uri: string;
-}
+export type DSNPMessage =
+  | DSNP.Broadcast
+  | DSNP.Reply
+  | DSNP.Drop
+  | DSNP.Reaction
+  | DSNP.GraphChange
+  | DSNP.KeyList
+  | DSNP.PrivateGraphKeyList
+  | DSNP.Private
+  | DSNP.EncryptionKeyList;
 
-/**
- * Reply: a public reply post
- */
-export interface DSNPReply {
-  /** fromAddress	ID of the sender	bytes20 */
-  fromAddress: EthereumAddress;
-  /** messageID	keccak-256 hash of content stored at uri	bytes32 */
-  messageID: Uint8Array;
-  /** inReplyTo	ID of the message the reply references	bytes32 */
-  inReplyTo: Uint8Array;
-  /** uri	content uri	string */
-  uri: string;
-}
+export namespace DSNP {
+  /**
+   * Broadcast: a public post
+   */
+  export interface Broadcast {
+    /** fromAddress	ID of the sender	bytes20 */
+    fromAddress: EthereumAddress;
+    /** messageID	keccak-256 hash of content stored at URI	bytes32 */
+    messageID: Uint8Array;
+    /** uri	content URI	string */
+    uri: string;
+  }
 
-/**
- * Drop: a dead drop message
- */
-export interface Drop {
-  /** deadDropID	The Dead Drop ID (See DeadDrops	bytes32) */
-  deadDropID: Uint8Array;
-  /** messageID	keccak-256 hash of content	bytes32 */
-  messageID: Uint8Array;
-  /** uri	content uri	string */
-  uri: string;
-}
+  /**
+   * Reply: a public reply post
+   */
+  export interface Reply {
+    /** fromAddress	ID of the sender	bytes20 */
+    fromAddress: EthereumAddress;
+    /** messageID	keccak-256 hash of content stored at uri	bytes32 */
+    messageID: Uint8Array;
+    /** inReplyTo	ID of the message the reply references	bytes32 */
+    inReplyTo: Uint8Array;
+    /** uri	content uri	string */
+    uri: string;
+  }
 
-/**
- * GraphChange: a public follow/unfollow
- */
-export interface GraphChange {
-  /** fromAddress	ID of the sender	bytes20 */
-  fromAddress: EthereumAddress;
-  /** actionType	follow/unfollow	number/enum */
-  actionType: number;
-}
+  /**
+   * Drop: a dead drop message
+   */
+  export interface Drop {
+    /** deadDropID	The Dead Drop ID (See DeadDrops	bytes32) */
+    deadDropID: Uint8Array;
+    /** messageID	keccak-256 hash of content	bytes32 */
+    messageID: Uint8Array;
+    /** uri	content uri	string */
+    uri: string;
+  }
 
-/**
- * KeyList, PrivateGraphKeyList, EncryptionKeyList
- */
-export interface KeyList {
-  /** fromAddress	ID of the sender	bytes20 */
-  keyList: Uint8Array[];
-  /** keyList	new list of valid keys	bytes[] */
-  fromAddress: EthereumAddress;
-}
+  /**
+   * GraphChange: a public follow/unfollow
+   */
+  export interface GraphChange {
+    /** fromAddress	ID of the sender	bytes20 */
+    fromAddress: EthereumAddress;
+    /** actionType	follow/unfollow	number/enum */
+    actionType: number;
+  }
 
-export interface PrivateGraphKeyList {
-  fromAddress: EthereumAddress;
-  keyList: Uint8Array[];
-}
+  /**
+   * KeyList, PrivateGraphKeyList, EncryptionKeyList
+   */
+  export interface KeyList {
+    /** fromAddress	ID of the sender	bytes20 */
+    keyList: Uint8Array[];
+    /** keyList	new list of valid keys	bytes[] */
+    fromAddress: EthereumAddress;
+  }
 
-export interface EncryptionKeyList {
-  fromAddress: EthereumAddress;
-  keyList: Uint8Array[];
-}
+  export interface PrivateGraphKeyList {
+    fromAddress: EthereumAddress;
+    keyList: Uint8Array[];
+  }
 
-/**
- * Inbox: direct message
- */
-export interface Inbox {
-  /** fromAddress	id of the sender	bytes20 */
-  fromAddress: EthereumAddress;
-  /** toAddress	ID of the recipient	bytes20 */
-  toAddress: EthereumAddress;
-  /** messageID	keccak-256 hash of content	bytes32 */
-  messageID: Uint8Array;
-  /** uri	content uri	string */
-  uri: string;
-}
+  export interface EncryptionKeyList {
+    fromAddress: EthereumAddress;
+    keyList: Uint8Array[];
+  }
 
-/** Encrypted Inbox:  an encrypted direct message: */
-export interface EncryptedInbox {
-  /** fromAddress	ID of the sender	bytes20 */
-  fromAddress: EthereumAddress;
-  /** toAddress	ID of the recipient	bytes20 */
-  toAddress: EthereumAddress;
-  /** messageID	keccak-256 hash of content	bytes32 */
-  messageID: Uint8Array;
-  /** uri	content uri	string */
-  uri: string;
-}
+  /**
+   * Inbox: direct message
+   */
+  export interface Inbox {
+    /** fromAddress	id of the sender	bytes20 */
+    fromAddress: EthereumAddress;
+    /** toAddress	ID of the recipient	bytes20 */
+    toAddress: EthereumAddress;
+    /** messageID	keccak-256 hash of content	bytes32 */
+    messageID: Uint8Array;
+    /** uri	content uri	string */
+    uri: string;
+  }
 
-/**
- * Reaction: a visual reply to a Broadcast message (aka post)
- */
-export interface Reaction {
-  /** fromAddress	id of the sender	bytes20 */
-  fromAddress: EthereumAddress;
-  /** emoji	the encoded reaction	number / UTF-8 bytes[] */
-  emoji: Uint8Array;
-  /** inReplyTo	ID of the message the reaction references	bytes32 */
-  inReplyTo: Uint8Array;
-}
+  /** Encrypted Inbox:  an encrypted direct message: */
+  export interface EncryptedInbox {
+    /** fromAddress	ID of the sender	bytes20 */
+    fromAddress: EthereumAddress;
+    /** toAddress	ID of the recipient	bytes20 */
+    toAddress: EthereumAddress;
+    /** messageID	keccak-256 hash of content	bytes32 */
+    messageID: Uint8Array;
+    /** uri	content uri	string */
+    uri: string;
+  }
 
-/**
- * Private: an encrypted message of unknown type
- */
-export interface Private {
-  /** ID of the sender */
-  fromAddress: EthereumAddress;
-  /* encrypted graph change data */
-  data: Uint8Array;
-  /** keccak-256 hash of unencrypted content */
-  messageID: Uint8Array;
+  /**
+   * Reaction: a visual reply to a Broadcast message (aka post)
+   */
+  export interface Reaction {
+    /** fromAddress	id of the sender	bytes20 */
+    fromAddress: EthereumAddress;
+    /** emoji	the encoded reaction	number / UTF-8 bytes[] */
+    emoji: Uint8Array;
+    /** inReplyTo	ID of the message the reaction references	bytes32 */
+    inReplyTo: Uint8Array;
+  }
+
+  /**
+   * Private: an encrypted message of unknown type
+   */
+  export interface Private {
+    /** ID of the sender */
+    fromAddress: EthereumAddress;
+    /* encrypted graph change data */
+    data: Uint8Array;
+    /** keccak-256 hash of unencrypted content */
+    messageID: Uint8Array;
+  }
 }
