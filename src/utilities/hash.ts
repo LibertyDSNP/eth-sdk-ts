ebf8ad6898419a4e76417b9e45916ed0cbc68b7d
Rose Karr <rose@rosalinekarr.com>
2021-04-02 09:09:57 -0400

    Updated files to use string types

--- src/utilities/hash.ts
+++ src/utilities/hash.ts
@@ -1,4 +1,6 @@
-export type KeccakHash = string;
+import { HexString } from "../types/Strings";
+
+export type KeccakHash = HexString;
 
 export const hashPrefix = (hash: string): KeccakHash => {
   return hash.startsWith("0x", 0) ? hash : `0x${hash}`;
