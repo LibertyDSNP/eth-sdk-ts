a4697584094bf18f8c1f7472af1d58236e49fb85
Rose Karr <rose@rosalinekarr.com>
2021-04-07 11:26:21 -0400

    Added more detail to the batch module stubs

--- src/batch/batch.ts
+++ src/batch/batch.ts
@@ -1,15 +1,25 @@
-import { ActivityPub } from "../activityPub/activityPub";
+import { DSNPMessage } from "../types/DSNP";
 import { NotImplementedError } from "../utilities/errors";
 
 export interface BatchFileObject {}
 
 /**
- * create() takes a series of activity pub objects and returns a Batch file
+ * createFile() takes a series of activity pub objects and returns a Batch file
  * object for publishing. This method is not yet implemented.
  *
- * @params  activityPubs  An array of activityPubs to include in the batch file
- * @returns               A batch file object
+ * @params  events  An array of DSNPEvent to include in the batch file
+ * @returns         A batch file object
  */
-export const create = (activityPubs: ActivityPub[]): BatchFileObject => {
+export const createFile = (events: DSNPMessage[]): BatchFileObject => {
+  throw NotImplementedError();
+};
+
+/**
+ * openFile() takes a Batch file object . This method is not yet implemented.
+ *
+ * @params  file  An array of DSNPEvent to include in the batch file
+ * @returns       A batch file object
+ */
+export const openFile = (file: BatchFileObject): DSNPMessage[] => {
   throw NotImplementedError();
 };
--- src/batch/queue.ts
+++ src/batch/queue.ts
@@ -1,14 +1,32 @@
-import { ActivityPub } from "../activityPub/activityPub";
+// import * as batch from "./batch";
+// import * as config from "../config/config";
+import { Config } from "../config/config";
+// import * as announcement from "../contracts/announcement";
+// import * as storage from "../storage/storage";
+import { DSNPMessage } from "../types/DSNP";
+import { NotImplementedError } from "../utilities/errors";
+import { KeccakHash } from "../utilities/hash";
+
+type QueueId = KeccakHash;
+
+export interface QueueInterface {
+  enqueue(dsnpEvent: DSNPMessage): Promise<QueueId>;
+  dequeue(id: QueueId): Promise<DSNPMessage>;
+  getAll(): Promise<DSNPMessage[]>;
+}
 
 /**
  * enqueue() adds an activity pub event to the queue for later publishing to the
  * blockchain as a batch file. This method is not yet implemented.
  *
- * @param   activityPub  The activity pub event to queue up for batching
- * @returns              An ID for the queued event
+ * @param   dsnpEvent  The DSNP event to queue up for batching
+ * @returns            An ID for the queued event
  */
-export const enqueue = (activityPub: ActivityPub): string => {
+export const enqueue = async (event: DSNPMessage, opts?: Config): Promise<QueueId> => {
   throw NotImplementedError();
+
+  // const config = config.getConfig(opts);
+  // return await config.queue.enqueue(event);
 };
 
 /**
@@ -16,16 +34,27 @@ export const enqueue = (activityPub: ActivityPub): string => {
  * to the blockchain. This method is not yet implemented.
  *
  * @param   id  The ID of the event to remove from the queue
- * @returns     The activity pub event removed from the queue
+ * @returns     The DSNP event event removed from the queue
  */
-export const dequeue = (id: string): ActivityPub => {
+export const dequeue = async (id: QueueId, opts?: Config): Promise<DSNPMessage> => {
   throw NotImplementedError();
+
+  // const config = config.getConfig(opts);
+  // return await config.queue.dequeue(id);
 };
 
 /**
  * commit() creates a batch file from the current activity pub events in the
  * queue then clears the queue. This method is not yet implemented.
  */
-export const commit = async () => {
+export const commit = async (opts?: Config) => {
   throw NotImplementedError();
+
+  // const config = config.getConfig(opts);
+  // const events = await config.queue.getAll();
+  // const batchFile = batch.createFile(events);
+  //
+  // const hash = batch.hash(batchFile);
+  // const uri = await storage.store(batchFile, opts);
+  // await announcement.batch(uri, hash);
 };
