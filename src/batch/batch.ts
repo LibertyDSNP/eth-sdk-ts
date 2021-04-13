bebd5667094bf59e0232e831537f5c75ef965a0d
Rose Karr <rose@rosalinekarr.com>
2021-04-13 08:08:19 -0400

    Updated docs for batch methods

--- src/batch/batch.ts
+++ src/batch/batch.ts
@@ -7,7 +7,7 @@ export type BatchFileObject = string;
  * createFile() takes a series of activity pub objects and returns a Batch file
  * object for publishing. This method is not yet implemented.
  *
- * @params  events  An array of DSNPEvent to include in the batch file
+ * @params  events  An array of DSNPEvents to include in the batch file
  * @returns         A batch file object
  */
 export const createFile = (_events: MessageType[]): BatchFileObject => {
@@ -17,8 +17,8 @@ export const createFile = (_events: MessageType[]): BatchFileObject => {
 /**
  * openFile() takes a Batch file object . This method is not yet implemented.
  *
- * @params  file  An array of DSNPEvent to include in the batch file
- * @returns       A batch file object
+ * @params  file  A batch file object to open
+ * @returns       An array of DSNPEvents from the batch file
  */
 export const openFile = (_file: BatchFileObject): MessageType[] => {
   throw NotImplementedError;
