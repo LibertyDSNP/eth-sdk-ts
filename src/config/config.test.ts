9cf2458cb5c487ff790af9c9fc10499eaa455b28
Rosaline Karr <rose@rosalinekarr.com>
2021-04-30 15:37:28 -0400

    Features/browser compatibility 177981081 (#19)
    * Removed file features from config module
    
    * Removed dotenv dependency from announcement module
    
    * Removed unnecessary package dependency
    
    * Removed unnecessary tests
    
    * Upgraded web3 to silence deprecation warnings
    
    * Updated package lock

--- src/config/config.test.ts
+++ src/config/config.test.ts
@@ -1,88 +1,40 @@
-import * as fs from "fs";
-import * as path from "path";
-
-import { Config } from "./config";
-
-// Require is necessary here to re-import the config module and reset local state
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-let config = require("./config");
-
-// module.export is necessary here because import is not supported in dynamic imports
-const TEST_CONFIG_FILE = `
-module.exports = { test: "object" };
-`;
+import { getConfig, setConfig, Config } from "./config";
 
 describe("config", () => {
   describe("#getConfig", () => {
-    describe("with a config file", () => {
-      const configPath = path.resolve("./dsnp.config.js");
+    it("returns the default settings when the config hasn't been set yet", async () => {
+      const results = await getConfig();
 
-      beforeAll(() => {
-        // Write test config to ./dsnp.config.js
-        fs.writeFileSync(configPath, TEST_CONFIG_FILE);
-      });
-
-      afterAll(() => {
-        // rm ./dsnp.config.js
-        fs.unlinkSync(configPath);
-
-        // Reimport config module to clear local state
-        jest.resetModules();
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
-        config = require("./config");
-      });
-
-      it("fetches the config settings from the file", async () => {
-        expect(await config.getConfig()).toMatchObject({ test: "object" });
-      });
-
-      it("overrides the returned settings with any provided parameters", async () => {
-        expect(
-          await config.getConfig(({
-            otherTest: "differentObject",
-          } as unknown) as Config)
-        ).toMatchObject({
-          test: "object",
-          otherTest: "differentObject",
-        });
-      });
+      // We have to test keys here because #toMatchObject doesn't work with
+      // object instances
+      expect(results["queue"]).toBeInstanceOf(Object);
+      expect(results["store"]).toBeInstanceOf(Object);
     });
 
-    describe("without a config file", () => {
-      it("returns the default settings when the config hasn't been set yet", async () => {
-        const results = await config.getConfig();
+    it("fetches the current config settings", async () => {
+      const testConfig = ({
+        test: "object",
+      } as unknown) as Config;
 
-        // We have to test keys here because #toMatchObject doesn't work with
-        // object instances
-        expect(results["queue"]).toBeInstanceOf(Object);
-        expect(results["store"]).toBeInstanceOf(Object);
-      });
+      await setConfig(testConfig);
 
-      it("fetches the current config settings", async () => {
-        const testConfig = ({
-          test: "object",
-        } as unknown) as Config;
+      expect(await getConfig()).toMatchObject({ test: "object" });
+    });
 
-        await config.setConfig(testConfig);
+    it("overrides the returned settings with any provided parameters", async () => {
+      const testConfig = ({
+        test: "object",
+      } as unknown) as Config;
 
-        expect(await config.getConfig()).toMatchObject({ test: "object" });
-      });
+      await setConfig(testConfig);
 
-      it("overrides the returned settings with any provided parameters", async () => {
-        const testConfig = ({
-          test: "object",
-        } as unknown) as Config;
-
-        await config.setConfig(testConfig);
-
-        expect(
-          await config.getConfig(({
-            otherTest: "differentObject",
-          } as unknown) as Config)
-        ).toMatchObject({
-          test: "object",
+      expect(
+        await getConfig(({
           otherTest: "differentObject",
-        });
+        } as unknown) as Config)
+      ).toMatchObject({
+        test: "object",
+        otherTest: "differentObject",
       });
     });
   });
@@ -93,9 +45,9 @@ describe("config", () => {
         test: "object",
       } as unknown) as Config;
 
-      await config.setConfig(testConfig);
+      await setConfig(testConfig);
 
-      expect(await config.getConfig()).toMatchObject({ test: "object" });
+      expect(await getConfig()).toMatchObject({ test: "object" });
     });
   });
 });
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,3 @@
-import * as fs from "fs";
-import * as path from "path";
 import Web3 from "web3";
 
 import MemoryQueue from "../batch/memoryQueue";
@@ -7,8 +5,6 @@ import { QueueInterface } from "../batch/queue";
 import MemoryStore from "../storage/memoryStorage";
 import { StorageInterface } from "../storage/storage";
 import { EthereumAddress } from "../types/Strings";
-
-const CONFIG_FILE_PATH = "./dsnp.config.js";
 
 export interface Config {
   accountAddress?: EthereumAddress;
@@ -22,20 +18,6 @@ let config: Config = {
   store: MemoryStore(),
 };
 
-let isConfigLoaded = false;
-const loadConfigFile = async (): Promise<void> => {
-  if (isConfigLoaded) return;
-
-  const filePath = path.resolve(CONFIG_FILE_PATH);
-  if (!fs.existsSync(filePath) || fs.lstatSync(filePath).isDirectory()) return;
-
-  // Load the config file from disk
-  config = await import(filePath);
-
-  // And set the flag to skip loading in the future
-  isConfigLoaded = true;
-};
-
 /**
  * getConfig() fetches the current configuration settings and returns them. This
  * method is not yet implemented.
@@ -43,13 +25,8 @@ const loadConfigFile = async (): Promise<void> => {
  * @returns The current configuration settings
  */
 export const getConfig = async (overrides?: Config): Promise<Config> => {
-  // Load the config file from disk, if any
-  await loadConfigFile();
-
-  // Return the config, if there are no overrides to apply
   if (!overrides) return config;
 
-  // Otherwise, return the config with overrides
   return { ...config, ...overrides };
 };
 
@@ -60,9 +37,5 @@ export const getConfig = async (overrides?: Config): Promise<Config> => {
  * @params newConfig The configuration settings to set with
  */
 export const setConfig = async (newConfig: Config): Promise<void> => {
-  // Set the config settings
   config = newConfig;
-
-  // Don't bother loading the config file in the future
-  isConfigLoaded = true;
 };
