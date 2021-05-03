203168d2a31c7eeef2642f51d53132c46d0f8cc1
Rose Karr <rose@rosalinekarr.com>
2021-05-03 09:57:34 -0400

    Implemented activity pub module

--- src/social/content.ts
+++ src/social/content.ts
@@ -1,7 +1,7 @@
 // import * as activityPub from "../activityPub/activityPub";
 // import * as config from "../config/config";
 // import * as storage from "../storage/storage";
-import { ActivityPubCreateOpts } from "../activityPub/activityPub";
+import { BroadcastOptions, ReactionOptions, ReplyOptions } from "../activityPub/activityPub";
 import { Config } from "../config/config";
 import { NotImplementedError } from "../utilities/errors";
 
@@ -9,11 +9,11 @@ import { NotImplementedError } from "../utilities/errors";
  * broadcast() creates a broadcast DSNP event and enqueues it for the next
  * batch. This method is not yet implemented.
  *
- * @param activityPubOpts  Any options for the activity pub event to create
- * @param opts             Optional. Configuration overrides, such as from address, if any
+ * @param content The content for the note to broadcast
+ * @param opts    Optional. Configuration overrides, such as from address, if any
  *
  */
-export const broadcast = async (_activityPubOpts: ActivityPubCreateOpts, _opts?: Config): Promise<void> => {
+export const broadcast = async (_content: BroadcastOptions, _opts?: Config): Promise<void> => {
   throw NotImplementedError;
 
   // const config = config.getConfig(opts);
@@ -30,10 +30,10 @@ export const broadcast = async (_activityPubOpts: ActivityPubCreateOpts, _opts?:
  * reply() creates a reply activity pub event and enqueues it for the next
  * batch. This method is not yet implemented.
  *
- * @param activityPubOpts  Any options for the activity pub event to create
- * @param opts             Optional. Configuration overrides, such as from address, if any
+ * @param content The content for the reply to broadcast
+ * @param opts    Optional. Configuration overrides, such as from address, if any
  */
-export const reply = async (_activityPubOpts: ActivityPubCreateOpts, _opts: Config): Promise<void> => {
+export const reply = async (_content: ReplyOptions, _opts: Config): Promise<void> => {
   throw NotImplementedError;
 
   // const config = config.getConfig(opts);
@@ -51,10 +51,10 @@ export const reply = async (_activityPubOpts: ActivityPubCreateOpts, _opts: Conf
  * react() creates a reaction activity pub event and enqueues it for the next
  * batch. This method is not yet implemented.
  *
- * @param activityPubOpts  Any options for the activity pub event to create
- * @param opts             Optional. Configuration overrides, such as from address, if any
+ * @param content The content for the reaction to broadcast
+ * @param opts    Optional. Configuration overrides, such as from address, if any
  */
-export const react = async (_activityPubOpts: ActivityPubCreateOpts, _opts: Config): Promise<void> => {
+export const react = async (_content: ReactionOptions, _opts: Config): Promise<void> => {
   throw NotImplementedError;
 
   // const config = config.getConfig(opts);
