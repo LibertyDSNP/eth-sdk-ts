3ee2fab68805f7d80946bb95b3ae1cf5a2a0725d
enddy <enddy.code@gmail.com>
2021-05-06 13:47:43 -0700

    feat(batch): add ability to read
    Add ability to read Parquet batch files
    and fetch bloom filters for column.
    
    This allows clients to quickly check
    if a batch file contains any relevant
    information before downloading.

--- src/batch/batch.ts
+++ src/batch/batch.ts
@@ -1,13 +1,31 @@
 import { MessageType } from "../types/DSNP";
 import { NotImplementedError } from "../utilities";
+import request from "request";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const parquet = require("@dsnp/parquetjs");
+const { ParquetReader } = parquet;
+
+type ReadRowFunction = {
+  (row: MessageType): void;
+};
+
+interface SplitBlockBloomFilter {
+  check(value: number | string): boolean;
+}
+
+interface BloomFilterData {
+  sbbf: SplitBlockBloomFilter;
+  columnName: string;
+  RowGroupIndex: number;
+}
 
 export type BatchFileObject = string;
 
 /**
- * createFile() takes a series of activity pub objects and returns a Batch file
+ * createFile() takes a series of DSNP messages and returns a Batch file
  * object for publishing. This method is not yet implemented.
  *
- * @params  events  An array of DSNPEvents to include in the batch file
+ * @param events  An array of DSNPEvents to include in the batch file
  * @returns         A batch file object
  */
 export const createFile = (_events: MessageType[]): BatchFileObject => {
@@ -15,11 +33,53 @@ export const createFile = (_events: MessageType[]): BatchFileObject => {
 };
 
 /**
- * openFile() takes a Batch file object . This method is not yet implemented.
+ * openURL() allows users to open a parquet file with a URL.
  *
- * @params  file  A batch file object to open
- * @returns       An array of DSNPEvents from the batch file
+ * @param url a URL to fetch parquet file from.
+ * @returns a ParquetReader object.
+ **/
+export const openURL = async (url: URL): Promise<typeof ParquetReader> => ParquetReader.openUrl(request, url);
+
+/**
+ * openFile() allows users to open a parquet file with a path.
+ *
+ * @param path to parquet file.
+ * @returns a ParquetReader object.
+ **/
+export const openFile = async (path: string): Promise<typeof ParquetReader> => ParquetReader.openFile(path);
+
+/**
+ * readFile() reads a Parquet file by row.
+ *
+ * @param reader a ParquetReader object.
+ * @returns void.
  */
-export const openFile = (_file: BatchFileObject): MessageType[] => {
-  throw NotImplementedError;
+export const readFile = async (reader: typeof ParquetReader, callback: ReadRowFunction): Promise<void> => {
+  const cursor = reader.getCursor();
+
+  let record = null;
+  while ((record = await cursor.next())) {
+    callback(record);
+  }
+
+  return reader.close();
+};
+
+/**
+ * batchIncludes() checks if a column/field in a
+ * Parquet batch file contains an item.
+ *
+ * @param  reader a ParquetReader object.
+ * @param  column the column name to check if a value is included.
+ * @param  item a value.
+ * @returns void.
+ */
+export const batchIncludes = async (
+  reader: typeof ParquetReader,
+  column: string,
+  item: number | string
+): Promise<boolean> => {
+  const bloomFilterData = await reader.getBloomFilters([column]);
+
+  return (bloomFilterData[column] || []).some((data: BloomFilterData) => data.sbbf.check(item));
 };
