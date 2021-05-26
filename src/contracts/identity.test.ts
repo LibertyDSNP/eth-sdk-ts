58a097554b7ba6fb7bdde06f133f31b9ee6454a2
Anna Neyzberg <anna@carbonfive.com>
2021-05-25 20:46:39 -0700

    add more test cases

--- src/contracts/identity.test.ts
+++ src/contracts/identity.test.ts
@@ -1,6 +1,6 @@
 //eslint-disable-next-line
 require("dotenv").config();
-import {ContractReceipt, ethers} from "ethers";
+import { ContractReceipt, ethers } from "ethers";
 
 import { snapshotHardhat, revertHardhat } from "../test/hardhatRPC";
 import { setConfig, getConfig } from "../config/config";
@@ -10,7 +10,7 @@ const RPC_URL = String(process.env.RPC_URL);
 const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
 const signer = new ethers.Wallet(TESTING_PRIVATE_KEY, provider);
 
-const owner = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
+const owner = "0x70997970c51812dc3a010c7d01b50e0d17dc79c8";
 
 beforeEach(async () => {
   const config = await getConfig();
@@ -28,21 +28,58 @@ afterEach(async () => {
 describe("identity", () => {
   describe("createCloneProxy", () => {
     it("creates a proxy contract", async () => {
-      const proxyReceipt: ContractReceipt = await (await createCloneProxy()).wait() as ContractReceipt;
-      const proxyContractEvents = proxyReceipt && proxyReceipt.events && proxyReceipt ? proxyReceipt.events.filter((event) => { return event.event === "ProxyCreated"}) : [];
+      const proxyReceipt: ContractReceipt = (await (await createCloneProxy()).wait()) as ContractReceipt;
+      console.log("prooxyReceipt1", proxyReceipt);
+      const proxyContractEvents =
+        proxyReceipt && proxyReceipt.events
+          ? proxyReceipt.events.filter((event) => {
+              return event.event === "ProxyCreated";
+            })
+          : [];
       const contractAddress = proxyContractEvents[0].args ? proxyContractEvents[0].args[0] : null;
-      console.log("contractAddress", contractAddress);
+
       await expect((await createCloneProxy()).wait()).resolves.not.toBeNull();
+      expect(contractAddress).toEqual("0x3B02fF1e626Ed7a8fd6eC5299e2C54e1421B626B");
     });
   });
 
   describe("createCloneProxy with owner", () => {
     it("creates a proxy contract", async () => {
-      const proxyReceipt: ContractReceipt = await (await createCloneProxyWithOwner(owner)).wait() as ContractReceipt;
-      const proxyContractEvents = proxyReceipt && proxyReceipt.events && proxyReceipt ? proxyReceipt.events.filter((event) => { return event.event === "ProxyCreated"}) : [];
+      const proxyReceipt: ContractReceipt = (await (await createCloneProxyWithOwner(owner)).wait()) as ContractReceipt;
+      console.log("proxyReceipt2", proxyReceipt);
+      const proxyContractEvents =
+        proxyReceipt && proxyReceipt.events && proxyReceipt
+          ? proxyReceipt.events.filter((event) => {
+              return event.event === "ProxyCreated";
+            })
+          : [];
       const contractAddress = proxyContractEvents[0].args ? proxyContractEvents[0].args[0] : null;
-      console.log("contractAddress", contractAddress);
       await expect((await createCloneProxy()).wait()).resolves.not.toBeNull();
+      expect(contractAddress).toEqual("0x3B02fF1e626Ed7a8fd6eC5299e2C54e1421B626B");
     });
+
+    // it("expect is authorized to to return true for owner", async () => {
+    //   const proxyReceipt: ContractReceipt = (await (await createCloneProxyWithOwner(owner)).wait()) as ContractReceipt;
+    //   console.log("proxyReceipt2", proxyReceipt);
+    //   const proxyContractEvents =
+    //     proxyReceipt && proxyReceipt.events && proxyReceipt
+    //       ? proxyReceipt.events.filter((event) => {
+    //         return event.event === "ProxyCreated";
+    //       })
+    //       : [];
+    //   const contractAddress = proxyContractEvents[0].args ? proxyContractEvents[0].args[0] : null;
+    // });
+    //
+    // it("expect is authorized to to return false for non owner", async () => {
+    //   const proxyReceipt: ContractReceipt = (await (await createCloneProxyWithOwner(owner)).wait()) as ContractReceipt;
+    //   console.log("proxyReceipt2", proxyReceipt);
+    //   const proxyContractEvents =
+    //     proxyReceipt && proxyReceipt.events && proxyReceipt
+    //       ? proxyReceipt.events.filter((event) => {
+    //         return event.event === "ProxyCreated";
+    //       })
+    //       : [];
+    //   const contractAddress = proxyContractEvents[0].args ? proxyContractEvents[0].args[0] : null;
+    // });
   });
 });
