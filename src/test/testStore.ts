1c5b4f4ccd323f2e37249c6fc522356fb007ebad
Rose Karr <rose@rosalinekarr.com>
2021-06-01 11:07:08 -0400

    Added a fake store for testing

---
+++ src/test/testStore.ts
@@ -0,0 +1,22 @@
+import { Content, File, StorageInterface } from "../storage/storage";
+
+export default class TestStore implements StorageInterface {
+  store: Record<string, Content>;
+
+  constructor() {
+    this.store = {};
+  }
+
+  async get(targetPath: string): Promise<File> {
+    return this.store[targetPath] as File;
+  }
+
+  async put(targetPath: string, content: Content): Promise<URL> {
+    this.store[targetPath] = content;
+    return new URL(`http://fakestore.org/${targetPath}`);
+  }
+
+  getStore(): Record<string, Content> {
+    return this.store;
+  }
+}
