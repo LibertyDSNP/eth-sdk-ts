e2b809d38706a9d253858e3bf672b55d17275f18
Wil Wade <wil.wade@unfinished.com>
2021-06-04 15:47:51 -0400

    Cherry pick cleanup

--- src/contracts/beaconfactory.ts
+++
@@ -1,7 +0,0 @@
-import { getContractAddress } from "./contract";
-import { HexString } from "../types/Strings";
-import { getConfig } from "../config/config";
-import { ContractTransaction } from "ethers";
-import { MissingContract, MissingProvider, MissingSigner } from "../utilities";
-import { BeaconFactory__factory } from "../types/typechain";
-
