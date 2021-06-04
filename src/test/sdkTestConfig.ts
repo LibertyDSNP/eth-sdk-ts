13c6e6d69e07d6603316de81f94287fe04697e6c
Wil Wade <wil.wade@unfinished.com>
2021-06-04 09:23:59 -0400

    Easy to use sdkTestConfig for test provider and signer

---
+++ src/test/sdkTestConfig.ts
@@ -0,0 +1,19 @@
+//eslint-disable-next-line
+require("dotenv").config();
+import { setConfig } from "../config/config";
+import { ethers } from "ethers";
+
+export const setupConfig = () => {
+  const TESTING_PRIVATE_KEY = String(process.env.TESTING_PRIVATE_KEY);
+  const RPC_URL = String(process.env.RPC_URL);
+  const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
+  const signer = new ethers.Wallet(TESTING_PRIVATE_KEY, provider);
+  setConfig({
+    signer,
+    provider,
+  });
+  return {
+    signer,
+    provider,
+  };
+};
