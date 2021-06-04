934d3651bdaa067be581c0a716d390eed1aa1845
Wil Wade <wil.wade@unfinished.com>
2021-06-04 09:23:34 -0400

    Setup the chain to snapshot and revert before/afterEach

--- src/test/chainSetup.ts
+++ src/test/chainSetup.ts
@@ -1,26 +1,18 @@
-//eslint-disable-next-line
+/* eslint @typescript-eslint/no-var-requires: "off" */
 require("dotenv").config();
-import { ethers, Signer } from "ethers";
-import { setConfig } from "../config/config";
+import { ethers } from "ethers";
 import { JsonRpcProvider } from "@ethersproject/providers/src.ts/json-rpc-provider";
 import { revertHardhat, snapshotHardhat } from "./hardhatRPC";
 
-const TESTING_PRIVATE_KEY = String(process.env.TESTING_PRIVATE_KEY);
 const RPC_URL = String(process.env.RPC_URL);
 
-let signer: Signer;
 let provider: JsonRpcProvider;
 
 beforeAll(async () => {
   provider = new ethers.providers.JsonRpcProvider(RPC_URL);
-  signer = new ethers.Wallet(TESTING_PRIVATE_KEY, provider);
 });
 
 beforeEach(async () => {
-  await setConfig({
-    provider,
-    signer,
-  });
   // Remember snapshots are used up each time they are reverted to, so beforeEach.
   await snapshotHardhat(provider);
 });
