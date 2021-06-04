819ca688c98baadb6dfa042c3f30b1f81fe2c27e
Wil Wade <wil.wade@unfinished.com>
2021-06-04 12:27:37 -0400

    Don't use @ethersproject

--- src/test/chainSetup.ts
+++ src/test/chainSetup.ts
@@ -1,12 +1,11 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
 require("dotenv").config();
 import { ethers } from "ethers";
-import { JsonRpcProvider } from "@ethersproject/providers/src.ts/json-rpc-provider";
 import { revertHardhat, snapshotHardhat } from "./hardhatRPC";
 
 const RPC_URL = String(process.env.RPC_URL);
 
-let provider: JsonRpcProvider;
+let provider: ethers.providers.JsonRpcProvider;
 
 beforeAll(async () => {
   provider = new ethers.providers.JsonRpcProvider(RPC_URL);
