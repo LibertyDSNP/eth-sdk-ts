d7936ede055ef47a403b99cc08468f79e53ea147
Wil Wade <wil.wade@unfinished.com>
2021-06-04 15:31:25 -0400

    Queue export

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,9 @@ export const contracts = contractsImport;
 import * as activityPubImport from "./activityPub";
 export const activityPub = activityPubImport;
 
+import * as queueImport from "./queue";
+export const queue = queueImport;
+
 import * as utilitiesImport from "./utilities";
 export const utilities = utilitiesImport;
 
