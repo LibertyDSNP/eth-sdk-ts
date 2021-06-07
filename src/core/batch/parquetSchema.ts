a430ef6136e45121fa354dd8a27c71ee17848d78
Wil Wade <wil.wade@unfinished.com>
2021-06-07 15:05:27 -0400

    Remove unused parquet schemas

--- src/core/batch/parquetSchema.ts
+++ src/core/batch/parquetSchema.ts
@@ -49,17 +49,6 @@ export const ReplyBloomFilterOptions = {
 };
 
 /**
- * Drop: a dead drop message
- */
-export const DropSchema = {
-  dsnpType: { type: "INT32" },
-  contentHash: { type: "BYTE_ARRAY" },
-  deadDropID: { type: "BYTE_ARRAY" },
-  uri: { type: "BYTE_ARRAY" },
-  signature: { type: "BYTE_ARRAY" },
-};
-
-/**
  * GraphChange: a public follow/unfollow
  * Note followType will be only 0 or 1; other values are invalid
  */
@@ -71,38 +60,11 @@ export const GraphChangeSchema = {
 };
 
 /**
- * KeyList, PrivateGraphKeyList, EncryptionKeyList
- * Note keyList field will need to have additional deserialization to convert to
- * a list of keys
- */
-export const KeyListSchema = {
-  dsnpType: { type: "INT32" },
-  fromId: { type: "BYTE_ARRAY" },
-  keyList: { type: "BYTE_ARRAY" },
-  signature: { type: "BYTE_ARRAY" },
-};
-
-export const PrivateKeyListSchema = KeyListSchema;
-export const EncryptionKeyListSchema = KeyListSchema;
-
-/**
  * Profile - a profile change message
  */
 export const ProfileSchema = {
   dsnpType: { type: "INT32" },
   fromId: { type: "BYTE_ARRAY" },
-  uri: { type: "BYTE_ARRAY" },
-  signature: { type: "BYTE_ARRAY" },
-};
-
-/**
- * Encrypted Inbox: an encrypted direct message:
- */
-export const EncryptedInboxSchema = {
-  dsnpType: { type: "INT32" },
-  contentHash: { type: "BYTE_ARRAY" },
-  fromId: { type: "BYTE_ARRAY" },
-  toAddress: { type: "BYTE_ARRAY" },
   uri: { type: "BYTE_ARRAY" },
   signature: { type: "BYTE_ARRAY" },
 };
@@ -120,15 +82,4 @@ export const ReactionSchema = {
 
 export const ReactionBloomFilterOptions = {
   bloomFilters: [{ column: "emoji" }, { column: "fromId" }, { column: "inReplyTo" }],
-};
-
-/**
- * Private: an encrypted message of unknown type
- */
-export const PrivateSchema = {
-  dsnpType: { type: "INT32" },
-  fromId: { type: "BYTE_ARRAY" },
-  data: { type: "BYTE_ARRAY" },
-  contentHash: { type: "BYTE_ARRAY" },
-  signature: { type: "BYTE_ARRAY" },
 };
