4d8d2a9576d61626c0085bf2a0cbdfea41271416
Wil Wade <wil.wade@unfinished.com>
2021-06-08 12:18:21 -0400

    Fix multimodule recursion

--- scripts/multimodules.js
+++ scripts/multimodules.js
@@ -28,7 +28,7 @@ const throwOnError = (err) => {
   if (err) throw new Error(err);
 };
 
-const jsTemplate = (name) => `
+const jsTemplate = (target, name) => `
 "use strict";
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
@@ -41,35 +41,71 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
 };
 exports.__esModule = true;
-__exportStar(require("./dist/cjs/${name}"), exports);
+__exportStar(require("${target ? [...Array(target.split("/").length)].map(() => "../") : "./"}dist/cjs/${[target, name]
+  .filter((x) => !!x)
+  .join("/")}"), exports);
 `;
 
-const typesTemplate = (name) => `export * from "./dist/types/${name}";`;
+const typesTemplate = (name, typePath) => `export * from "${typePath.trim("/")}/${name}";`;
 
-const createMultiModule = ([moduleName, modulePath]) => {
+const createMultiModule = (targetPath, typesPath) => ([moduleName, modulePath]) => {
   if (!moduleName || !modulePath) {
     throw new Error(`${moduleName} from ${modulePath} is broken somehow...`);
   }
 
   console.log(`Processing Multimodule support for ${moduleName}...`);
-
   // Types
-  fs.writeFileSync(`${path.join(__dirname, "..", moduleName + ".d.ts")}`, typesTemplate(modulePath), throwOnError);
+  fs.writeFileSync(
+    `${path.join(__dirname, "..", targetPath, moduleName + ".d.ts")}`,
+    typesTemplate(modulePath, typesPath),
+    throwOnError
+  );
   // commonjs
-  fs.writeFileSync(`${path.join(__dirname, "..", moduleName + ".js")}`, jsTemplate(modulePath), throwOnError);
+  fs.writeFileSync(
+    `${path.join(__dirname, "..", targetPath, moduleName + ".js")}`,
+    jsTemplate(targetPath, modulePath),
+    throwOnError
+  );
 };
 
-const fileLines = fs.readFileSync(`${path.join(__dirname, "../src/index.ts")}`, "UTF-8").split(/\r?\n/);
+const multimoduleFile = (relativeFileAndPath, targetPath, typesPath) => {
+  const fileLines = fs.readFileSync(`${path.join(__dirname, relativeFileAndPath)}`, "UTF-8").split(/\r?\n/);
 
-const importAsToExport = Object.fromEntries(
-  fileLines
-    .map((x) => x.match(/^export const (.*?) = (.*?);/))
+  try {
+    fs.mkdirSync(path.join(__dirname, "..", targetPath));
+  } catch (e) {
+    if (!e.message.match(/file already exists/)) throw e;
+  }
+
+  const importAsToExport = Object.fromEntries(
+    fileLines
+      .map((x) => x.match(/^export const (.*?) = (.*?);/))
+      .filter((x) => !!x)
+      .map((x) => [x[2], x[1]])
+  );
+
+  const recurse = fileLines
+    .map((x) => x.match(/^import.* as (.*?) from.*"\.\/(.*?)"/))
     .filter((x) => !!x)
-    .map((x) => [x[2], x[1]])
-);
+    .filter((x) => !!importAsToExport[x[1]]) // Ignore things that are not re-exported
+    .map((x) => [importAsToExport[x[1]], x[2]]);
 
-fileLines
-  .map((x) => x.match(/^import.* as (.*?) from.*"\.\/(.*?)"/))
-  .filter((x) => !!x)
-  .map((x) => [importAsToExport[x[1]], x[2]])
-  .map(createMultiModule);
+  recurse.map(createMultiModule(targetPath, typesPath));
+  return recurse;
+};
+
+const queue = [""];
+
+let item;
+while ((item = queue.pop()) !== undefined) {
+  const relativeFileAndPath = ["..", "src", item, "index.ts"].filter((x) => !!x).join("/");
+
+  if (!fs.existsSync(`${path.join(__dirname, relativeFileAndPath)}`)) continue;
+
+  const targetHeight = !item ? 0 : [...item].filter((x) => x === "/").length + 1;
+  const pathBackout = [...Array(targetHeight)].map(() => "..");
+  const typePath = [".", ...pathBackout, "dist", "types", item].filter((x) => !!x).join("/");
+
+  const recurse = multimoduleFile(relativeFileAndPath, item, typePath);
+  if (recurse) recurse.forEach((x) => queue.push([item, x[0]].filter((y) => !!y).join("/")));
+}
