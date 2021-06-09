0a326bebaca5a8733dc3c4d2ff3054b5ef670965
Wil Wade <wil.wade@unfinished.com>
2021-06-09 12:41:56 -0400

    Config adds provider to the signer if it doesn't exist

--- src/test/sdkTestConfig.ts
+++ src/test/sdkTestConfig.ts
@@ -1,21 +1,22 @@
 //eslint-disable-next-line
 require("dotenv").config();
 import { setConfig } from "../config";
-import { ethers } from "ethers";
+import { Wallet, Signer, providers } from "ethers";
 
-type SdkTestConfig = { provider: ethers.providers.JsonRpcProvider; signer: ethers.Signer };
+type SdkTestConfig = { provider: providers.Provider; signer: Signer };
 
 export const setupConfig = (): SdkTestConfig => {
   const TESTING_PRIVATE_KEY = String(process.env.TESTING_PRIVATE_KEY);
   const RPC_URL = String(process.env.RPC_URL);
-  const provider = new ethers.providers.JsonRpcProvider(RPC_URL);
-  const signer = new ethers.Wallet(TESTING_PRIVATE_KEY);
-  setConfig({
+  const provider = new providers.JsonRpcProvider(RPC_URL);
+  const signer = new Wallet(TESTING_PRIVATE_KEY);
+  const conf = setConfig({
     signer,
     provider,
   });
+  if (!conf.signer || !conf.provider) throw new Error("Something is very wrong with sdkTestConfig");
   return {
-    signer,
-    provider,
+    signer: conf.signer,
+    provider: conf.provider,
   };
 };
