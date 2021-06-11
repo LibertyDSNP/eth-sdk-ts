d940372b25ee7f0ba47c6ba453ef66d39ed42571
shannonwells <shannonwells@users.noreply.github.com>
2021-06-10 17:07:46 -0700

    missed a test?

--- src/core/contracts/identity.test.ts
+++ src/core/contracts/identity.test.ts
@@ -13,7 +13,7 @@ import {
 import { EthAddressRegex } from "../../test/matchers";
 import { setupConfig } from "../../test/sdkTestConfig";
 import { snapshotSetup } from "../../test/hardhatRPC";
-import { MissingContract } from "../utilities";
+import { MissingContract } from "../../config";
 
 const owner = "0x70997970c51812dc3a010c7d01b50e0d17dc79c8";
 const nonOwner = "0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc";
