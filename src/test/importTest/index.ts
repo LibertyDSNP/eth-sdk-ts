09aba713eeaa595b585d45ccdc1f894be917bef7
Wil Wade <wil.wade@unfinished.com>
2021-06-14 17:01:03 -0400

    Add a test for basic project imports

---
+++ src/test/importTest/index.ts
@@ -0,0 +1,14 @@
+import assert from "assert";
+
+import sdk from "@dsnp/sdk";
+import { generators } from "@dsnp/sdk";
+
+Object.entries({
+  // Add imports that should exist
+  sdk,
+  generators,
+}).forEach(([key, value]) => {
+  assert.notStrictEqual(value, undefined, `Was unable to import ${key}`);
+})
+
+assert(sdk.setConfig({}));
