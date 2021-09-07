e594a6e7e06e9a6d27ce6fa7339b44e2061a2bd3
Wil Wade <wil.wade@unfinished.com>
2021-09-07 09:58:02 -0400

    Fix typechain better

--- scripts/package.js
+++ scripts/package.js
@@ -14,17 +14,13 @@ delete rootPackage["jest-junit"];
 delete rootPackage["jest"];
 
 // Don't keep scripts
-rootPackage["scripts"] = {
-  "prepare": "typechain --target=ethers-v5 ./node_modules/@dsnp/contracts/abi/**/*.json --out-dir ./types/typechain"
-};
+delete rootPackage["scripts"];
 
 // Setup the main and types correctly
 rootPackage["main"] = "index.js";
 rootPackage["types"] = "index.d.ts";
 
 // Write it out
-fs.writeFileSync(
-  `${path.join(__dirname, "../dist", "package.json")}`,
-  JSON.stringify(rootPackage, null, 2),
-  (err) => { if (err) throw new Error(err); }
-);
+fs.writeFileSync(`${path.join(__dirname, "../dist", "package.json")}`, JSON.stringify(rootPackage, null, 2), (err) => {
+  if (err) throw new Error(err);
+});
